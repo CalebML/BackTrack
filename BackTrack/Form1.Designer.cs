540c8438cd79c45efef92fda76514a9dfa12dc9b
Caleb Larson <LarsonCaleb1991@gmail.com>
2015-11-18 17:24:37 -0800

    Sprint 2

--- BackTrack/Form1.Designer.cs
+++ BackTrack/Form1.Designer.cs
@@ -30,6 +30,10 @@
         {
             this.ReadData = new System.Windows.Forms.Button();
             this.clearSDCard = new System.Windows.Forms.Button();
+            this.MainMap = new GMap.NET.WindowsForms.GMapControl();
+            this.ZoomIn = new System.Windows.Forms.Button();
+            this.ZoomOut = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // ReadData
@@ -44,7 +48,7 @@
             // 
             // clearSDCard
             // 
-            this.clearSDCard.Location = new System.Drawing.Point(182, 12);
+            this.clearSDCard.Location = new System.Drawing.Point(108, 12);
             this.clearSDCard.Name = "clearSDCard";
             this.clearSDCard.Size = new System.Drawing.Size(90, 44);
             this.clearSDCard.TabIndex = 1;
@@ -52,16 +56,84 @@
             this.clearSDCard.UseVisualStyleBackColor = true;
             this.clearSDCard.Click += new System.EventHandler(this.clearSDCard_Click);
             // 
+            // MainMap
+            // 
+            this.MainMap.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.MainMap.Bearing = 0F;
+            this.MainMap.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.MainMap.CanDragMap = true;
+            this.MainMap.EmptyTileColor = System.Drawing.Color.Navy;
+            this.MainMap.GrayScaleMode = false;
+            this.MainMap.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
+            this.MainMap.LevelsKeepInMemmory = 5;
+            this.MainMap.Location = new System.Drawing.Point(12, 62);
+            this.MainMap.MarkersEnabled = true;
+            this.MainMap.MaxZoom = 18;
+            this.MainMap.MinZoom = 2;
+            this.MainMap.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            this.MainMap.Name = "MainMap";
+            this.MainMap.NegativeMode = false;
+            this.MainMap.PolygonsEnabled = true;
+            this.MainMap.RetryLoadTile = 0;
+            this.MainMap.RoutesEnabled = true;
+            this.MainMap.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
+            this.MainMap.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
+            this.MainMap.ShowTileGridLines = false;
+            this.MainMap.Size = new System.Drawing.Size(483, 305);
+            this.MainMap.TabIndex = 2;
+            this.MainMap.Zoom = 0D;
+            // 
+            // ZoomIn
+            // 
+            this.ZoomIn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.ZoomIn.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ZoomIn.Location = new System.Drawing.Point(376, 13);
+            this.ZoomIn.Name = "ZoomIn";
+            this.ZoomIn.Size = new System.Drawing.Size(47, 43);
+            this.ZoomIn.TabIndex = 3;
+            this.ZoomIn.Text = "+";
+            this.ZoomIn.UseVisualStyleBackColor = true;
+            this.ZoomIn.Click += new System.EventHandler(this.ZoomIn_Click);
+            // 
+            // ZoomOut
+            // 
+            this.ZoomOut.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.ZoomOut.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ZoomOut.Location = new System.Drawing.Point(430, 13);
+            this.ZoomOut.Name = "ZoomOut";
+            this.ZoomOut.Size = new System.Drawing.Size(44, 43);
+            this.ZoomOut.TabIndex = 4;
+            this.ZoomOut.Text = "-";
+            this.ZoomOut.UseVisualStyleBackColor = true;
+            this.ZoomOut.Click += new System.EventHandler(this.ZoomOut_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(333, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Zoom:";
+            // 
             // BackTrack
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(507, 379);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ZoomOut);
+            this.Controls.Add(this.ZoomIn);
+            this.Controls.Add(this.MainMap);
             this.Controls.Add(this.clearSDCard);
             this.Controls.Add(this.ReadData);
             this.Name = "BackTrack";
             this.Text = "BackTrack - The Software!";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -69,6 +141,13 @@
 
         private System.Windows.Forms.Button ReadData;
         private System.Windows.Forms.Button clearSDCard;
+        private GMap.NET.WindowsForms.GMapControl MainMap;
+        private GMap.NET.WindowsForms.GMapOverlay Points;
+        private GMap.NET.WindowsForms.GMapOverlay Route;
+        private System.Windows.Forms.Button ZoomIn;
+        private System.Windows.Forms.Button ZoomOut;
+        private System.Windows.Forms.Label label1;
+        //GMapOverlay test
     }
 }
 
--- BackTrack/Form1.cs
+++ BackTrack/Form1.cs
@@ -10,12 +10,26 @@ using System.Windows.Forms;
 using System.IO;
 using System.Runtime.InteropServices;
 using Microsoft.Win32.SafeHandles;
+using GMap;
+using GMap.NET;
+using GMap.NET.MapProviders;
+using GMap.NET.WindowsForms;
+using GMap.NET.CacheProviders;
+using GMap.NET.Internals;
+using GMap.NET.ObjectModel;
+using GMap.NET.Projections;
+using System.Management;
+using System.Management.Instrumentation;
+
 
 namespace BackTrack
 {
     
     public partial class BackTrack : Form
     {
+        //data for the hike
+        LocPoint[] hike;
+
         //function import (setting up "CreateFile")
         [DllImport("Kernel32.dll", SetLastError = true, CharSet = CharSet.Auto)]
         static extern SafeFileHandle CreateFile(
@@ -78,6 +92,71 @@ namespace BackTrack
         public BackTrack()
         {
             InitializeComponent();
+
+            try
+            {
+                System.Net.IPHostEntry e =
+                     System.Net.Dns.GetHostEntry("www.google.com");
+            }
+            catch
+            {
+                MainMap.Manager.Mode = AccessMode.CacheOnly;
+                MessageBox.Show("No internet connection avaible, going to CacheOnly mode.",
+                      "Warning", MessageBoxButtons.OK,
+                      MessageBoxIcon.Warning);
+            }
+
+            // config map
+            //MainMap.MapProvider = GMapProviders.OpenStreetMap;
+            MainMap.MapProvider = GMapProviders.GoogleTerrainMap;
+            MainMap.Position = new PointLatLng(45.604590, -122.807256);
+            MainMap.MinZoom = 0;
+            MainMap.MaxZoom = 24;
+            MainMap.Zoom = 11;
+
+            MainMap.OnMarkerClick += new MarkerClick(MarkerClick);
+            //MainMap.OnMarkerEnter += new MarkerEnter(MainMap_OnMarkerEnter);
+            //MainMap.OnMarkerLeave += new MarkerLeave(MainMap_OnMarkerLeave);
+
+            /**************************************
+            GMapOverlay test = new GMapOverlay();
+            PointLatLng[] points = new PointLatLng[2];
+            points[0].Lat = 45.604590;
+            points[0].Lng = -122.807256;
+
+            points[1].Lat = 45.604590;
+            points[1].Lng = -122.807;
+
+            test.Markers.Add(new GMap.NET.WindowsForms.Markers.GMarkerGoogle(points[0], GMap.NET.WindowsForms.Markers.GMarkerGoogleType.green));
+            test.Markers.Add(new GMap.NET.WindowsForms.Markers.GMarkerGoogle(points[1], GMap.NET.WindowsForms.Markers.GMarkerGoogleType.green));
+            MainMap.Overlays.Add(test);
+
+
+            //MapRoute route = new MapRoute(test.Markers.GetEnumerator(), "test");
+            GMapRoute route = new GMapRoute(points, "test");
+            test.Routes.Add(route);
+            ****************************************/
+        }
+
+        private string GetSDDriveNum()
+        {
+            //find the SD card drive number
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher("SELECT * FROM Win32_DiskDrive");
+            string path = "\\\\\\\\.\\\\physicaldrive1";
+            foreach (ManagementObject wmi_HD in searcher.Get())
+            {
+                string DriveModel = wmi_HD["Model"].ToString();
+                if ((DriveModel == "SDHC Card") | DriveModel == "SD Card")
+                    path = wmi_HD.Path.ToString();
+            }
+
+            //Remove escape characters from device ID
+            string[] splitPath;
+            splitPath = path.Split('\"');
+            string drive = splitPath[1].Remove(0, 2);
+            drive = drive.Remove(3, 1);
+
+            return drive;
         }
 
         private void ReadData_Click(object sender, EventArgs e)
@@ -87,14 +166,25 @@ namespace BackTrack
             byte[] sdAddr = new byte[32];
             int intSdAddr = 0;
 
+            string drive = GetSDDriveNum();
+
             //Read the current address, This tells us how many data "sets" we need to read
             //we need the first 4 bytes (32-bits)
             //ReadDrive("E:\\", 32);                    //This must be ran as admin
-            sdAddr = ReadDrive("\\\\.\\physicaldrive1", 32);     //this does not
+            //sdAddr = ReadDrive("\\\\.\\physicaldrive1", 32);     //this does not
+            sdAddr = ReadDrive(drive, 32);
+            int shift = 5;
+
+            for (int i = 0; i < 4; i++)
+            {
+                sdAddr[i] = (byte)(sdAddr[i] >> shift);
+                sdAddr[i] = (byte)((sdAddr[i + 1] << (8 - shift)) + sdAddr[i]);
+            }
+
             for (int i = 0; i < 4; i++)
             {
                 intSdAddr = intSdAddr << 8;
-                intSdAddr += sdAddr[i];
+                intSdAddr += sdAddr[3-i];
             }
 
             byte[] sdData = new byte[intSdAddr * 32];
@@ -102,7 +192,8 @@ namespace BackTrack
 
             //read data from SD card
             int bytesToRead = ((intSdAddr * 32) + 32);
-            sdData = ReadDrive("\\\\.\\physicaldrive1", bytesToRead);
+            //sdData = ReadDrive("E:\\", bytesToRead);
+            sdData = ReadDrive(drive, bytesToRead);
 
             for (int i = bytesToRead; i>32; i--)
             {
@@ -111,7 +202,8 @@ namespace BackTrack
             //CharSdData.Reverse();
 
             //create an array of LocPoints (Location Points)
-            LocPoint[] hike = new LocPoint[intSdAddr];
+            hike = new LocPoint[intSdAddr];
+
 
             //handle each location point
             for (int i = 0; i < intSdAddr; i++)
@@ -172,15 +264,107 @@ namespace BackTrack
                 hike[i].capTime.seconds = hike[i].capTime.seconds * 10;
                 hike[i].capTime.seconds += (int)CharSdData[(i * 32) + 29] - '0';
             }
+
+            AddPointsToMap();
+            MainMap.Refresh();
+        }
+
+        private void AddPointsToMap()
+        {
+            if(Points == null)
+            {
+                Points = new GMapOverlay();
+            }
+            if(Route == null)
+            {
+                Route = new GMapOverlay();
+            }
+
+            //remove old points
+            MainMap.Overlays.Remove(Points);
+
+            //add route
+            Route.Routes.Add(new GMapRoute("Route1"));
+            GMapRoute r = new GMapRoute("hikeRoute");
+
+            //keep track of where the map needs to show once the points are added
+            double highLat = -100, lowLat = 100, highLon = -200, lowLon = 200;
+
+            for (int i = 0; i < hike.Length; i++)
+            {
+                double lat = hike[i].degreeNorthSouth;
+                lat += (hike[i].minuteNorthSouth / 60);
+                if (hike[i].northSouth == 'S')
+                {
+                    lat = lat * -1;
+                }
+
+                double lon = hike[i].degreeEastWest;
+                lon += (hike[i].minuteEastWest / 60);
+                if (hike[i].eastWest == 'W')
+                {
+                    lon = lon * -1;
+                }
+
+                //modify high and low values
+                if (lat > highLat)
+                {
+                    highLat = lat;
+                }
+                if(lat < lowLat)
+                {
+                    lowLat = lat;
+                }
+
+                if (lon > highLon)
+                {
+                    highLon = lon;
+                }
+                if (lon < lowLon)
+                {
+                    lowLon = lon;
+                }
+
+                PointLatLng pos = new PointLatLng(lat, lon);
+                //Route.Routes.Add()
+                r.Points.Add(pos);
+
+                
+                //add "tool tips" and create marker
+                GMapMarker Marker = new GMap.NET.WindowsForms.Markers.GMarkerGoogle(pos, GMap.NET.WindowsForms.Markers.GMarkerGoogleType.green);
+                //Marker.ToolTip = new GMapToolTip(Marker);
+                Marker.ToolTipText = "Point Number: " + (hike.Length - i)
+                                        + "\nTime: " + hike[i].capTime.hours + ":" + hike[i].capTime.minutes + ":" + hike[i].capTime.seconds + " UTC"
+                                        + "\nTime Hiked: " //+ (hike[i].capTime.hours - hike[0].capTime.hours) + ":" + (hike[i].capTime.minutes - hike[0].capTime.minutes) + ":" + (hike[i].capTime.seconds - hike[0].capTime.seconds)
+                                        + "\nLatitude: " + hike[i].degreeNorthSouth + "º" + hike[i].minuteNorthSouth + "'" + hike[i].northSouth
+                                        + "\nLongitude: " + hike[i].degreeEastWest + "º" + hike[i].minuteEastWest + "'" + hike[i].eastWest;
+
+                //Marker.IsHitTestVisible = false;      //makes marker unclickable and tool tips wont show up
+                
+
+                Points.Markers.Add(Marker);
+            }
+            Route.Routes.Add(r);
+            MainMap.Overlays.Add(Route);
+
+            MainMap.Overlays.Add(Points);
+
+            //move the map
+            MainMap.Position = new PointLatLng((highLat + lowLat)/2, (highLon + lowLon)/2);
+
         }
 
         private void clearSDCard_Click(object sender, EventArgs e)
         {
             //set up array of 0 bytes to clear address section of SD card
-            byte[] clearIndex = Enumerable.Repeat((byte)0x00, 32).ToArray();
-                //new byte[32];
-            //clearIndex[1] = 0x00;
-            writeToDisk("\\\\.\\physicaldrive1", clearIndex);
+            byte[] clearIndex = new byte[1537];
+            for(int i = 0; i<1537; i++)
+            {
+                clearIndex[i] = (byte)0x00;
+            }
+            clearIndex[0] = (byte)0x01;
+
+            writeToDisk(GetSDDriveNum(), clearIndex);
         }
 
         //function to read 
@@ -247,6 +431,24 @@ namespace BackTrack
 
             try { diskStreamToWrite.Close(); } catch { }
             try { drive.Close(); } catch { }
+        }
+
+        private void ZoomIn_Click(object sender, EventArgs e)
+        {
+            if (MainMap.Zoom + 1 < MainMap.MaxZoom)
+                MainMap.Zoom += 1;
+        }
+
+        private void ZoomOut_Click(object sender, EventArgs e)
+        {
+            if (MainMap.Zoom - 1 > MainMap.MinZoom)
+                MainMap.Zoom -= 1;
+        }
+
+        private void MarkerClick(object sender, EventArgs e)
+        {
+            MainMap.BringToFront(); //for breakpoint
+            //Points.Markers.
         }
     }
 }
